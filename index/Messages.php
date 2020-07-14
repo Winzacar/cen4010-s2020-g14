1f74d61f832003e2c6e8a2ab0bc6406e7db8a033
Adam H Fox <43577499+ahf724@users.noreply.github.com>
2020-07-14 18:36:52 -0400

    Update Messages.php

--- index/Messages.php
+++ index/Messages.php
@@ -8,24 +8,6 @@
     $message = filter_input(INPUT_POST, 'message');
 
 
-    $host = '127.0.0.1';
-
-    try {
-        $user = 'smedina2017';
-        $pass = '';
-        $link = new PDO('mysql:host=127.0.0.1;port=8889;dbname=smedina2017', trim($user), 'Greatgrad3$');
-        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Successfully submitted';
-        $link = null;
-    }
-    catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-    
-  
-
-
-
-  
+   
 
 ?>
