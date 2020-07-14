addf8f15d326df6855bc191641659fd4622a8717
Adam H Fox <43577499+ahf724@users.noreply.github.com>
2020-07-14 18:28:21 -0400

    Update Messages.php

--- index/Messages.php
+++ index/Messages.php
@@ -12,7 +12,7 @@
 
     try {
         $user = 'smedina2017';
-        $pass = 'Greatgrad3$';
+        $pass = '';
         $link = new PDO('mysql:host=127.0.0.1;port=8889;dbname=smedina2017', trim($user), 'Greatgrad3$');
         $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo 'Successfully submitted';
@@ -22,47 +22,10 @@
         echo "Connection failed: " . $e->getMessage();
     }
     
-   
-
-   /** 
-   
-   $db_user = 'root';
-    $db_pass = '';
-    $db_name = 'smedinda2017';
-    
-   $link = new mysqli($host, $db_user, $db_pass, $db_name);
-   
-   
-   mysql_connect("127.0.0.1", "root", "") or die(mysql_error());
-    mysql_select_db("FNB1C_data") or die(mysql_error());**/
+  
 
 
 
-
-
-
-    /**define('DB_NAME', 'smedina2017');
-    define('DB_USER', 'smedina2017');
-    define('DB_PASSWORD', 'Greatgrad3$');
-    define('DB_HOST', 'localhost');
-
-    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
-    if (!$link) {
-        die ('Could not connect: ' . mysql_error());
-    }
-
-    $db_selected= mysql_select_db(DB_NAME, $link);
-    if (!$db_selected) {
-        die('Can\'t use ' . DB_NAME . ':' . mysql_error());
-    }
-
-    $fname = $_REQUEST['fname'];
-    $lname = $_REQUEST['lname'];
-    $email = $_REQUEST['email'];
-    $phone = $_REQUEST['phone'];
-    $reason = $_REQUEST['reason'];
-    $message = $_REQUEST['message'];**/
-    
-    
+  
 
 ?>
